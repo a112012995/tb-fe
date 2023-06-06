9f4094d66f77ccf50a56d43049f81c84f98cfdd4
Ahmad Fajar Kusumajati <111202012995@mhs.dinus.ac.id>
2023-06-06 14:45:21 +0700

    Added routes

---
+++ src/pages/Details.jsx
@@ -0,0 +1,13 @@
+import React from 'react'
+
+const Details = () => {
+  return (
+    <>
+        <div className=''>
+            <p>Details</p>
+        </div>
+    </>
+  )
+}
+
+export default Details
--- src/pages/index.js
+++ src/pages/index.js
@@ -1,4 +1,6 @@
 import Login from './Login'
 import Home from './Home'
+import Details from './Details'
+import Dashboard from './Dashboard'
 
-export { Login, Home }
+export { Login, Home, Details, Dashboard }

0fb7d8905ba3b41e15db9b993477c8ca79218ab0
Ahmad Fajar Kusumajati <111202012995@mhs.dinus.ac.id>
2023-06-07 23:34:20 +0700

    Changed App.js

--- src/App.js
+++ src/App.js
@@ -1,13 +1,14 @@
 import React from "react";
 import { BrowserRouter, Routes, Route } from "react-router-dom";
 import { Login, Details, Dashboard } from "./pages";
+import Hmm from "./pages/Hmm";
 
 function App() {
   return (
     <BrowserRouter>
       <Routes>
         <Route path="/" element={<Login />} />
-        <Route path="/details" element={<Details />} />
+        <Route path="/details" element={<Hmm />} />
         <Route path="/dashboard" element={<Dashboard />} />
       </Routes>
     </BrowserRouter>

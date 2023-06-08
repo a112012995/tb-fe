49fe697b99b6b3d1140dfbf503ac2da1f9900111
Ahmad Fajar Kusumajati <111202012995@mhs.dinus.ac.id>
2023-06-08 10:54:55 +0700

    Added Map component

--- src/pages/Details.jsx
+++ src/pages/Details.jsx
@@ -1,7 +1,17 @@
 import React from "react";
+import Map from "../components/Map";
 
 const Details = () => {
-  return <div>Details</div>;
+  return (
+    <>
+      <div className="flex flex-col items-center justify-center h-screen">
+        <h1 className="text-3xl font-bold mb-4">Details Page</h1>
+        <div className="w-full h-96 md:w-2/3 lg:w-1/2">
+          <Map />
+        </div>
+      </div>
+    </>
+  );
 };
 
 export default Details;

9e182f6baa8a540c3c744b5b65906f0cfd6e69a4
Caesardi11 <ardicaesarkurniawan@gmail.com>
2023-06-13 23:32:34 +0700

    feat: mapping and table components in dashboard-page

--- src/App.js
+++ src/App.js
@@ -1,19 +1,17 @@
-import React from "react";
-import { BrowserRouter, Routes, Route } from "react-router-dom";
-import { Login, Details, Dashboard } from "./pages";
-import Hmm from "./pages/Hmm";
+import React from 'react';
+import { BrowserRouter, Routes, Route } from 'react-router-dom';
+import { Login, Details, Dashboard } from './pages';
 
 function App() {
-  return (
-    <BrowserRouter>
-      <Routes>
-        <Route path="/" element={<Login />} />
-        <Route path="/hmm" element={<Hmm />} />
-        <Route path="/details" element={<Details />} />
-        <Route path="/dashboard" element={<Dashboard />} />
-      </Routes>
-    </BrowserRouter>
-  );
+	return (
+		<BrowserRouter>
+			<Routes>
+				<Route path="/" element={<Login />} />
+				<Route path="/details" element={<Details />} />
+				<Route path="/dashboard" element={<Dashboard />} />
+			</Routes>
+		</BrowserRouter>
+	);
 }
 
 export default App;

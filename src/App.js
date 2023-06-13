a0d461c9cf2afadbada72ef6dedbd097402aa479
Atha Ardisa <ardisaatha1@gmail.com>
2023-06-13 11:06:01 +0700

    coba

--- src/App.js
+++ src/App.js
@@ -1,14 +1,14 @@
 import React from "react";
 import { BrowserRouter, Routes, Route } from "react-router-dom";
 import { Login, Details, Dashboard } from "./pages";
-import Hmm from "./pages/Hmm";
+// import Hmm from "./pages/Hmm";
 
 function App() {
   return (
     <BrowserRouter>
       <Routes>
         <Route path="/" element={<Login />} />
-        <Route path="/hmm" element={<Hmm />} />
+        {/* <Route path="/hmm" element={<Hmm />} /> */}
         <Route path="/details" element={<Details />} />
         <Route path="/dashboard" element={<Dashboard />} />
       </Routes>

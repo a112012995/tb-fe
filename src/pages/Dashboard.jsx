305034bc85203494356d81647bea131d63a150b1
Caesardi11 <ardicaesarkurniawan@gmail.com>
2023-06-16 15:41:06 +0700

    memperbarui tampilan ui dashboard

--- src/pages/Dashboard.jsx
+++ src/pages/Dashboard.jsx
@@ -1,79 +1,52 @@
 import Peta from '../components/Peta';
 import Tabel from '../components/Tabel';
 import Navbar from '../components/navbar';
-import { useState } from 'react';
+import bgHero from '../assets/bg-hero.png';
+import iconTb from '../assets/icon-tb.png';
+import Stats from '../components/Stats';
+import Map from '../components/Map';
 
 const Dashboard = () => {
-	const [changeStatus, setChangeStatus] = useState('0');
-
-	const renderStatus = () => {
-		if (changeStatus === '1') return <Peta />;
-		else if (changeStatus === '2') return <Tabel />;
-		return <Peta />;
-	};
-
 	return (
-		<div className="bg-[#F5EFE7]">
-			<Navbar />
-			<div className="px-12 pt-24 text-black ">
-				<div className=" text-2xl font-bold">Ringkasan Data</div>
-				<div className="flex gap-10 mt-5 ">
-					<div className="card w-96 glass">
-						<div className="card-body">
-							<div className="stat">
-								<div className="stat-title ">Total Page Views</div>
-								<div className="stat-value">89,400</div>
-							</div>
-							<div className="card-actions justify-end">
-								<div className="stat-desc">21% more than last month</div>
-								<button className="btn btn-primary">Learn now!</button>
-							</div>
+		<div className="bg-[#F6F6F6]">
+			<div>
+				<div
+					className="flex flex-col min-h-screen text-white bg-center bg-cover bg-blend-overlay bg-fixed bg-black/30"
+					style={{
+						backgroundImage: `url(${bgHero})`,
+					}}
+				>
+					<Navbar />
+					<div className="pt-16 px-40">
+						<div>
+							<div className="text-4xl font-bold text-[#FFB800]">Selamat Datang,</div>
+							<div className="text-6xl font-bold">Sistem Distribusi Kerentanan Penyakit Tuberkulosis</div>
 						</div>
 					</div>
-					<div className="card w-96 glass">
-						<div className="card-body">
-							<h2 className="card-title">Life hack</h2>
-							<p>How to park your car at your garage?</p>
-							<div className="card-actions justify-end">
-								<button className="btn btn-primary">Learn now!</button>
-							</div>
-						</div>
-					</div>
-					<div className="card w-96 glass">
-						<div className="card-body">
-							<h2 className="card-title">Life hack</h2>
-							<p>How to park your car at your garage?</p>
-							<div className="card-actions justify-end">
-								<button className="btn btn-primary">Learn now!</button>
-							</div>
-						</div>
-					</div>
-					<div className="card w-96 glass">
-						<div className="card-body">
-							<h2 className="card-title">Life hack</h2>
-							<p>How to park your car at your garage?</p>
-							<div className="card-actions justify-end">
-								<button className="btn btn-primary">Learn now!</button>
+				</div>
+				<div className="flex space-x-8 justify-center items-center flex-col">
+					<div className="-mt-[75px] card rounded-3xl w-fit bg-[#4F709C] drop-shadow-lg">
+						<div className="flex pb-4 pt-3 px-10 flex-col">
+							<div>
+								<Stats />
 							</div>
 						</div>
 					</div>
 				</div>
-				<div className="mt-40  text-2xl font-bold">Grafik Data</div>
-				<div className="flex gap-10 mt-4 justify-between">
-					<img src="/grafik1.png" alt="grafik 1" />
-					<img src="/grafik2.png" alt="grafik 2" />
-					<img src="/grafik3.png" alt="grafik 3" />
+			</div>
+			<div className="px-20 mt-32 text-black ">
+				<div className="flex justify-center items-center gap-5 px-20">
+					<img src={iconTb} alt="icon tb" className="w-[500px] " />
+					<p className="text-justify text-lg">
+						Tuberkulosis (TB) adalah penyakit infeksi yang disebabkan oleh bakteri Mycobacterium tuberculosis. Penyakit ini umumnya menyerang paru-paru, tetapi juga dapat menyerang organ lain seperti ginjal, tulang, dan otak. Gejala utama
+						TB paru meliputi batuk kronis, demam, kelelahan, dan penurunan berat badan. Penularan TB terjadi melalui udara saat seseorang yang terinfeksi batuk atau bersin. Penanganan TB melibatkan pengobatan jangka panjang dengan
+						antibiotik untuk membasmi bakteri penyebabnya.
+					</p>
 				</div>
-				<div className="mt-16">
-					<button className="btn btn-error" onClick={() => setChangeStatus('1')}>
-						Peta
-					</button>
-					<button className="btn btn-error ml-5" onClick={() => setChangeStatus('2')}>
-						Tabel
-					</button>
-					<div className="mt-7 pb-5">{renderStatus()}</div>
+				<div className="w-full mt-32">
+					<Map />
 				</div>
-				<div></div>
+				<div className="mt-8 bg-[#E3F2FD] rounded-lg py-4 pl-4 border border-[#1565C0]">sssssssssssssss</div>
 			</div>
 		</div>
 	);

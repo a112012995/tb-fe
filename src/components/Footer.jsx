2f80effd2ee5dd8fe2caae4879146ad45bce33ad
isamsd26 <91447964+isamsd26@users.noreply.github.com>
2023-06-21 20:33:47 +0700

    update footer

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -6,8 +6,8 @@ const Footer = () => {
 		<div className="items-center text-center pt-52 pb-5">
 			© Copyright by Tim TB
 			<div className="flex justify-center items-center mt-3">
-				<img src={logoUdinus} alt="logo UDINUS" className="w-[70px] h-[70px]" />
-				<img src="/logo_dkk.png" alt="logo" className="w-24 h-23" />
+				<img src={logoUdinus} alt="logo UDINUS" className="w-[100px] h-[100px]" />
+				<img src="/logo_dkk.png" alt="logo" className="w-28 h-24" />
 			</div>
 		</div>
 	);

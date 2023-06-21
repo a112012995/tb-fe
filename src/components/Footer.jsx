aa52cbd77669900828de0fbe358bebc0f380096c
isamsd26 <91447964+isamsd26@users.noreply.github.com>
2023-06-21 20:27:18 +0700

    update footerr

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -1,4 +1,4 @@
-import logoUdinus from '../assets/logo-udinus.png';
+import logoUdinus from "../assets/logo-udinus.png";
 
 
 const Footer = () => {
@@ -6,8 +6,8 @@ const Footer = () => {
 		<div className="items-center text-center pt-52 pb-5">
 			© Copyright by Tim TB
 			<div className="flex justify-center items-center mt-3">
-				<img src={logoUdinus} alt="logo Udinus" className="w-[70px] h-[70px]" />
-				<img src="/logo_dkk.png" alt="logo udinus" className="w-28 h-24" />
+				<img src={logoUdinus} alt="logo UDINUS" className="w-[70px] h-[70px]" />
+				<img src="/logo_dkk.png" alt="logo" className="w-24 h-23" />
 			</div>
 		</div>
 	);

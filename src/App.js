0b49abc3c5316dc858436f6467e454697bf06eed
Atha Ardisa <ardisaatha1@gmail.com>
2023-06-22 23:20:11 +0700

    fixing route

--- src/App.js
+++ src/App.js
@@ -6,9 +6,9 @@ function App() {
   return (
     <BrowserRouter>
       <Routes>
-        <Route path="/" element={<Login />} />
+        <Route path="/login" element={<Login />} />
         <Route path="/details" element={<Details />} />
-        <Route path="/BeforeLogin" element={<BeforeLogin />} />
+        <Route path="/" element={<BeforeLogin />} />
         <Route path="/dashboard" element={<Dashboard />} />
       </Routes>
     </BrowserRouter>

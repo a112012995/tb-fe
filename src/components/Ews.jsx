bc9558ca4997cb44f33070f75f35aac4ba1dba8a
Atha Ardisa <ardisaatha1@gmail.com>
2023-09-28 15:17:53 +0700

    add percentage from new api

---
+++ src/components/Ews.jsx
@@ -0,0 +1,65 @@
+import React from "react";
+import { getLocationById } from "../store/actions/location";
+import { useSelector } from "react-redux";
+
+const Ews = () => {
+  const { survei } = useSelector((state) => state.locationReducers);
+  console.log(survei);
+  return (
+    <div className="grid grid-cols-3 px-52 mt-16 gap-5">
+      <div className="p-6 rounded-md border-2 bg-white w-full space-y-8">
+        <h4 className="font-semibold text-2xl">Literasi</h4>
+        <div className="flex space-x-6">
+          <div
+            className="radial-progress text-[#1565C0]"
+            style={{
+              "--value": `${Math.round(survei.persentase_literasi * 100)}`,
+              "--size": "8rem",
+              "--thickness": "0.75rem",
+            }}
+          ></div>
+          <div className="place-self-center space-y-4">
+            <h2 className="text-[#1CD1A1] text-6xl font-semibold ">{Math.round(survei.persentase_literasi * 100)}%</h2>
+            <h4 className="font-bold">{survei.avg_literasi}</h4>
+          </div>
+        </div>
+      </div>
+      <div className="p-6 rounded-md border-2 bg-white w-full space-y-8">
+        <h4 className="font-semibold text-2xl">Stigma</h4>
+        <div className="flex space-x-6">
+          <div
+            className="radial-progress text-[#1565C0]"
+            style={{
+              "--value": `${Math.round(survei.persentase_stigma * 100)}`,
+              "--size": "8rem",
+              "--thickness": "0.75rem",
+            }}
+          ></div>
+          <div className="place-self-center space-y-4">
+            <h2 className="text-[#1CD1A1] text-6xl font-semibold ">{Math.round(survei.persentase_stigma * 100)}%</h2>
+            <h4 className="font-bold">{survei.avg_stigma}</h4>
+          </div>
+        </div>
+      </div>
+      <div className="p-6 rounded-md border-2 bg-white w-full space-y-8">
+        <h4 className="font-semibold text-2xl">Pengetahuan</h4>
+        <div className="flex space-x-6">
+          <div
+            className="radial-progress text-[#1565C0]"
+            style={{
+              "--value": `${Math.round(survei.persentase_pengetahuan * 100)}`,
+              "--size": "8rem",
+              "--thickness": "0.75rem",
+            }}
+          ></div>
+          <div className="place-self-center space-y-4">
+            <h2 className="text-[#1CD1A1] text-6xl font-semibold ">{Math.round(survei.persentase_pengetahuan * 100)}%</h2>
+            <h4 className="font-bold">{survei.avg_pengetahuan}</h4>
+          </div>
+        </div>
+      </div>
+    </div>
+  );
+};
+
+export default Ews;
--- src/pages/Details.jsx
+++ src/pages/Details.jsx
@@ -1,139 +1,202 @@
-import React, { useEffect } from 'react';
-import OrderTerms from '../components/Accordion';
-import Footer from '../components/Footer';
-import TableDet from '../components/TabelDet';
-import { useLocation, useNavigate } from 'react-router-dom';
-import { getLocationById } from '../store/actions/location';
-import { useDispatch, useSelector } from 'react-redux';
-import { logout } from '../store/actions/auth';
-import { getPasienByIdKel } from '../store/actions/pasien';
+import React, { useEffect } from "react";
+import OrderTerms from "../components/Accordion";
+import Footer from "../components/Footer";
+import TableDet from "../components/TabelDet";
+import { useLocation, useNavigate } from "react-router-dom";
+import { getLocationById } from "../store/actions/location";
+import { useDispatch, useSelector } from "react-redux";
+import { logout } from "../store/actions/auth";
+import { getPasienByIdKel } from "../store/actions/pasien";
+import Ews from "../components/Ews";
 
 const Details = () => {
-	const { state } = useLocation();
-	const dispatch = useDispatch();
-	const history = useNavigate();
-	useEffect(() => {
-		dispatch(getLocationById(state.areaId));
-		dispatch(getPasienByIdKel(state.areaId));
-	}, [dispatch, state]);
-	const { dataById, totalPas } = useSelector((state) => state.locationReducers);
-	const { dataByIdKel } = useSelector((state) => state.pasienReducers);
-	console.log(dataByIdKel);
-	let sembuh = {};
-	let gagal = {};
-	let meninggal = {};
-	if (dataByIdKel) {
-		sembuh = dataByIdKel.filter((data) => data.hasil_akhir === 'Sembuh');
-		gagal = dataByIdKel.filter((data) => data.hasil_akhir === 'Gagal');
-		meninggal = dataByIdKel.filter((data) => data.hasil_akhir === 'Meninggal');
-	}
-	return (
-		<div className="bg-[#F6F6F6] m-auto items-center justify-center max-w-screen min-h-screen">
-			{/* Navbar */}
-			<div className="px-20 pt-12 pb-9 bg-[#213555] text-white">
-				<div className="navbar ">
-					<div className="navbar-start">
-						<div className="flex items-center">
-							<div>
-								<img src="/logo_dkk.png" alt="logo" className="w-28 h-24" />
-							</div>
-							<div className="flex-col">
-								<div className="font-semibold text-2xl">SDKPT</div>
-								<div className="font-semibold text-2xl">Kota Semarang</div>
-							</div>
-						</div>
-					</div>
-					<div className="navbar-end gap-12">
-						<a href="/dashboard" className="text-2xl font-semibold">
-							Home
-						</a>
-						<button>
-							<div className="text-2xl font-semibold scroll-smooth" onClick={() => dispatch(logout(history))}>
-								Logout
-							</div>
-						</button>
-					</div>
-				</div>
-			</div>
+  const { state } = useLocation();
+  const dispatch = useDispatch();
+  const history = useNavigate();
+  useEffect(() => {
+    dispatch(getLocationById(state.areaId));
+    dispatch(getPasienByIdKel(state.areaId));
+  }, [dispatch, state]);
+  const { dataById, totalPas } = useSelector((state) => state.locationReducers);
+  const { dataByIdKel } = useSelector((state) => state.pasienReducers);
+  console.log(dataByIdKel);
+  let sembuh = {};
+  let gagal = {};
+  let meninggal = {};
+  if (dataByIdKel) {
+    sembuh = dataByIdKel.filter((data) => data.hasil_akhir === "Sembuh");
+    gagal = dataByIdKel.filter((data) => data.hasil_akhir === "Gagal");
+    meninggal = dataByIdKel.filter((data) => data.hasil_akhir === "Meninggal");
+  }
+  return (
+    <div className="bg-[#F6F6F6] m-auto items-center justify-center max-w-screen min-h-screen">
+      {/* Navbar */}
+      <div className="px-20 pt-12 pb-9 bg-[#213555] text-white">
+        <div className="navbar ">
+          <div className="navbar-start">
+            <div className="flex items-center">
+              <div>
+                <img src="/logo_dkk.png" alt="logo" className="w-28 h-24" />
+              </div>
+              <div className="flex-col">
+                <div className="font-semibold text-2xl">SDKPT</div>
+                <div className="font-semibold text-2xl">Kota Semarang</div>
+              </div>
+            </div>
+          </div>
+          <div className="navbar-end gap-12">
+            <a href="/dashboard" className="text-2xl font-semibold">
+              Home
+            </a>
+            <button>
+              <div
+                className="text-2xl font-semibold scroll-smooth"
+                onClick={() => dispatch(logout(history))}
+              >
+                Logout
+              </div>
+            </button>
+          </div>
+        </div>
+      </div>
 
-			{/* Daerah */}
-			<div className="py-12 px-40">
-				<div>
-					<div className="text-4xl font-bold text-black">Detail Daerah {dataById.nama_kelurahan}</div>
-				</div>
-			</div>
+      {/* Daerah */}
+      <div className="py-12 px-40">
+        <div>
+          <div className="text-4xl font-bold text-black">
+            Detail Daerah {dataById.nama_kelurahan}
+          </div>
+        </div>
+      </div>
 
-			{/* Stats */}
-			<div className="flex space-x-8 justify-center items-center flex-col">
-				<div className="rounded-3xl w-fit bg-[#FFFFFF] drop-shadow-lg">
-					<div className="flex pb-4 pt-3 px-10 flex-col">
-						<div>
-							<div className="stats bg-[#FFFFFF] ">
-								<div className="stat">
-									<div className="flex">
-										<div className="flex-col text-center  pt-2">
-											<div className="stat-value text-black font-bold text-5xl">{totalPas}</div>
-											<div className="stat-title text-black text-lg">Jumlah Kasus</div>
-										</div>
-										<div className="text-warning">
-											<svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" className="inline-block w-8 h-8 stroke-current">
-												<path strokeLinecap="round" strokeLinejoin="round" strokeWidth="2" d="M13 16h-1v-4h-1m1-4h.01M21 12a9 9 0 11-18 0 9 9 0 0118 0z"></path>
-											</svg>
-										</div>
-									</div>
-								</div>
+      {/* Stats */}
+      <div className="flex space-x-8 justify-center items-center flex-col">
+        <div className="rounded-3xl w-fit bg-[#FFFFFF] drop-shadow-lg">
+          <div className="flex pb-4 pt-3 px-10 flex-col">
+            <div>
+              <div className="stats bg-[#FFFFFF] ">
+                <div className="stat">
+                  <div className="flex">
+                    <div className="flex-col text-center  pt-2">
+                      <div className="stat-value text-black font-bold text-5xl">
+                        {totalPas}
+                      </div>
+                      <div className="stat-title text-black text-lg">
+                        Jumlah Kasus
+                      </div>
+                    </div>
+                    <div className="text-warning">
+                      <svg
+                        xmlns="http://www.w3.org/2000/svg"
+                        fill="none"
+                        viewBox="0 0 24 24"
+                        className="inline-block w-8 h-8 stroke-current"
+                      >
+                        <path
+                          strokeLinecap="round"
+                          strokeLinejoin="round"
+                          strokeWidth="2"
+                          d="M13 16h-1v-4h-1m1-4h.01M21 12a9 9 0 11-18 0 9 9 0 0118 0z"
+                        ></path>
+                      </svg>
+                    </div>
+                  </div>
+                </div>
 
-								<div className="stat">
-									<div className="flex">
-										<div className="flex-col text-center  pt-2">
-											<div className="stat-value text-black font-bold text-5xl">{sembuh.length}</div>
-											<div className="stat-title text-black text-lg">Pasien Sembuh</div>
-										</div>
-										<div className="text-warning">
-											<svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" className="inline-block w-8 h-8 stroke-current">
-												<path strokeLinecap="round" strokeLinejoin="round" strokeWidth="2" d="M13 16h-1v-4h-1m1-4h.01M21 12a9 9 0 11-18 0 9 9 0 0118 0z"></path>
-											</svg>
-										</div>
-									</div>
-								</div>
+                <div className="stat">
+                  <div className="flex">
+                    <div className="flex-col text-center  pt-2">
+                      <div className="stat-value text-black font-bold text-5xl">
+                        {sembuh.length}
+                      </div>
+                      <div className="stat-title text-black text-lg">
+                        Pasien Sembuh
+                      </div>
+                    </div>
+                    <div className="text-warning">
+                      <svg
+                        xmlns="http://www.w3.org/2000/svg"
+                        fill="none"
+                        viewBox="0 0 24 24"
+                        className="inline-block w-8 h-8 stroke-current"
+                      >
+                        <path
+                          strokeLinecap="round"
+                          strokeLinejoin="round"
+                          strokeWidth="2"
+                          d="M13 16h-1v-4h-1m1-4h.01M21 12a9 9 0 11-18 0 9 9 0 0118 0z"
+                        ></path>
+                      </svg>
+                    </div>
+                  </div>
+                </div>
 
-								<div className="stat">
-									<div className="flex">
-										<div className="flex-col text-center  pt-2">
-											<div className="stat-value text-black font-bold text-5xl">{gagal.length}</div>
-											<div className="stat-title text-black text-lg">Pengobatan Gagal</div>
-										</div>
-										<div className="text-warning">
-											<svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" className="inline-block w-8 h-8 stroke-current">
-												<path strokeLinecap="round" strokeLinejoin="round" strokeWidth="2" d="M13 16h-1v-4h-1m1-4h.01M21 12a9 9 0 11-18 0 9 9 0 0118 0z"></path>
-											</svg>
-										</div>
-									</div>
-								</div>
+                <div className="stat">
+                  <div className="flex">
+                    <div className="flex-col text-center  pt-2">
+                      <div className="stat-value text-black font-bold text-5xl">
+                        {gagal.length}
+                      </div>
+                      <div className="stat-title text-black text-lg">
+                        Pengobatan Gagal
+                      </div>
+                    </div>
+                    <div className="text-warning">
+                      <svg
+                        xmlns="http://www.w3.org/2000/svg"
+                        fill="none"
+                        viewBox="0 0 24 24"
+                        className="inline-block w-8 h-8 stroke-current"
+                      >
+                        <path
+                          strokeLinecap="round"
+                          strokeLinejoin="round"
+                          strokeWidth="2"
+                          d="M13 16h-1v-4h-1m1-4h.01M21 12a9 9 0 11-18 0 9 9 0 0118 0z"
+                        ></path>
+                      </svg>
+                    </div>
+                  </div>
+                </div>
 
-								<div className="stat">
-									<div className="flex">
-										<div className="flex-col text-center  pt-2">
-											<div className="stat-value text-black font-bold text-5xl">{meninggal.length}</div>
-											<div className="stat-title text-black text-lg">Pasien Meninggal</div>
-										</div>
-										<div className="text-warning">
-											<svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" className="inline-block w-8 h-8 stroke-current">
-												<path strokeLinecap="round" strokeLinejoin="round" strokeWidth="2" d="M13 16h-1v-4h-1m1-4h.01M21 12a9 9 0 11-18 0 9 9 0 0118 0z"></path>
-											</svg>
-										</div>
-									</div>
-								</div>
-							</div>
-						</div>
-					</div>
-				</div>
-			</div>
-			<TableDet />
-			<Footer />
-			{/* Intervensi */}
-		</div>
-	);
+                <div className="stat">
+                  <div className="flex">
+                    <div className="flex-col text-center  pt-2">
+                      <div className="stat-value text-black font-bold text-5xl">
+                        {meninggal.length}
+                      </div>
+                      <div className="stat-title text-black text-lg">
+                        Pasien Meninggal
+                      </div>
+                    </div>
+                    <div className="text-warning">
+                      <svg
+                        xmlns="http://www.w3.org/2000/svg"
+                        fill="none"
+                        viewBox="0 0 24 24"
+                        className="inline-block w-8 h-8 stroke-current"
+                      >
+                        <path
+                          strokeLinecap="round"
+                          strokeLinejoin="round"
+                          strokeWidth="2"
+                          d="M13 16h-1v-4h-1m1-4h.01M21 12a9 9 0 11-18 0 9 9 0 0118 0z"
+                        ></path>
+                      </svg>
+                    </div>
+                  </div>
+                </div>
+              </div>
+            </div>
+          </div>
+        </div>
+      </div>
+      <Ews />
+      <TableDet />
+      <Footer />
+      {/* Intervensi */}
+    </div>
+  );
 };
 
 export default Details;

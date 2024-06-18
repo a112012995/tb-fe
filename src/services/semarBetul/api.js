0336d053244e33a6a49dc1601da33cfdd7273266
Atha Ardisa <92588716+ardisaatha@users.noreply.github.com>
2024-06-18 20:19:39 +0700

    add interceptor for exp token handling

--- src/services/semarBetul/api.js
+++ src/services/semarBetul/api.js
@@ -1,5 +1,6 @@
 import axios from "axios";
-// import store from "../store";
+import { logout } from "../../store/actions/auth";
+import store from "../../store";
 
 const API = axios.create({
   baseURL: process.env.REACT_APP_SB_URL,
@@ -9,14 +10,23 @@ const API = axios.create({
   },
 });
 
+// Pass location as an argument to the interceptor function
+const interceptor = (err, location) => {
+  console.log(err);
+  if (err.response.status === 401) {
+    store.dispatch(logout(location));
+    throw err;
+  }
+};
+
 API.interceptors.response.use(
   (res) => {
+    console.log(res);
     return res;
   },
-  (err) => {
-    if (err.response.status !== 401) {
-      throw err;
-    }
+  async (err) => {
+    // Pass the location information to the interceptor function
+    interceptor(err, window.location.assign("/"));
   }
 );
 
